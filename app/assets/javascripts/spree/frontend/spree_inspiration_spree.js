b214874cc00e2468e8e328a2fd33e518e5842d94
acreilly <acreilly3@gmail.com>
2015-02-01 14:58:44 -0500

    file name change, some styling

--- app/assets/javascripts/spree/frontend/spree_inspiration_spree.js
+++ app/assets/javascripts/spree/frontend/spree_inspiration_spree.js
@@ -8,20 +8,20 @@ jQuery(function() {
       .filter('.posts:first').show().end()
       .end()
 
-    .find('span:lt(2)').addClass('open').text('▼').end()  
+    .find('span:lt(2)').addClass('open').text('▼').end()
     .find('span:gt(1)').text('►').end()
-    
-    $('#news-archive span').click(function() {  
+
+    $('#news-archive span').click(function() {
        if ($(this).hasClass('open'))
         {
           $(this).toggleClass('open').text('►');
-        } 
+        }
         else
         {
           $(this).toggleClass('open').text('▼');
         }
-        
-        $(this).siblings('ul:first').toggle(); 
-     });                           
+
+        $(this).siblings('ul:first').toggle();
+     });
 });
 
